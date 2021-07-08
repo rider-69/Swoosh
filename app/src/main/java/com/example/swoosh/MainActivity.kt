f55740b67edc7389f62fa9c702cfb40b7e1b774f
kunal <kunalv1234567890@gmail.com>
2021-07-08 11:31:27 +0530

    Layout page 1

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcomeactivity)
+
+         val getStarted:Button = findViewById(R.id.GetStartedbtn)
+        getStarted.setOnClickListener {
+               val LeagueIntent =  Intent(this,LeagueActivity::class.java )
+              startActivity(LeagueIntent)
+        }
     }
 }
